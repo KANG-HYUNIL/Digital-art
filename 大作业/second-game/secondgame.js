8e1616d8dc295547e4fb769ae367cec001d6de89
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-06-20 00:39:11 +0900

    practice for game2, find some image
    find turtle, dog, background image and wrote image load codes. Still need to write jump and bird code

--- 大作业/second-game/secondgame.js
+++ 大作业/second-game/secondgame.js
@@ -4,17 +4,20 @@ const canvas = document.getElementById('canvas');
 const cvs = canvas.getContext('2d');
 
 //example canvas width, height
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight - 100;
+canvas.width = 1200;
+canvas.height = 800;
 
 //get image
 const player = new Image();
 const dog = new Image();
 const bird = new Image();
+const background = new Image();
 
-player.src = "";
-dog.src = "";
+player.src = "../images/turtle for game2.png";
+dog.src = "../images/dog for game2.png";
 bird.src = "";
+background.src = "../images/background for game2.jpg"
+
 
 var timer = 0;
 const clearTime = 30;
@@ -22,21 +25,23 @@ const clearTime = 30;
 var enemys = [];
 var enemySpeed = 3;
 
-var playerWidth;
-var dogWidth;
-var birdWidth;
+var playerWidth = 100;
+var dogWidth = 200;
+var birdWidth = 200;
 
 var playerSize;
-var playerX;
-var playerY;
+var playerX = 0;
+var playerY = 600;
 
 var dogSize;
-var dogSpawnX;
-var dogSpawnY;
+var dogSpawnX = 1500;
+var dogSpawnY = 600;
 
 var birdSize;
 var birdSpawnX;
 var birdSpawnY;
+
+var backGroundSize;
 
 //change imgs size
 function changeSize(img, setWidth){
@@ -67,6 +72,11 @@ bird.onload = function() {
     birdSize = changeSize(bird, birdWidth);
 
 }
+
+background.onload = function(){
+    backGroundSize = changeSize(background, canvas.width)
+}
+
 
 function drawPlayer(){
 
@@ -120,6 +130,7 @@ class Enemy{
 function moveObject(){
 
     cvs.clearRect(0, 0, canvas.width, canvas.height);
+    cvs.drawImage(background, 0, -backGroundSize.height + canvas.height, backGroundSize.width, backGroundSize.height);
 
     drawPlayer();
     enemys.forEach(function (e) {
@@ -165,24 +176,26 @@ function frameActive(){
 
     moveObject();
 
-    if (timer % 120 === 0){
+    if (timer % 240 === 0){
 
         random = Math.random() * 2;
 
         if (random <= 1){
-            enemys.push(new Enemy(dog, dogSpawnX, dogSpawnY, dogSize.width, dogSize.height));
+            enemys.push(new Enemy(dog, dogSpawnX, dogSpawnY, dogSize));
         }
 
         else{
-            enemys.push(new Enemy(bird, birdSpawnX, birdSpawnY, birdSize.width, birdSize.height));
+            enemys.push(new Enemy(bird, birdSpawnX, birdSpawnY, birdSize));
         }       
     }
 
-    if (timer % 300 === 0){
-        enemySpeed += 2;
+    if (timer % 600 === 0){
+        enemySpeed += 0.5;
     }
 
 }//frameActive 
+
+cvs.clearRect(0, 0, canvas.width, canvas.height);
 
 frameActive();
 
