ff4f8900a8af01ddf7190a74408af100981349d8
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2024-07-02 17:43:07 +0900

    fixing intro codes
    for evert introduction page except the first one, add white square frame will contain texts and set its position bottom(to increase the visibility of background)

---
+++ 大作业/fourth-introduction/fourthintro.js
@@ -0,0 +1,72 @@
+
+let typeWrite = document.querySelector(".TypeWrite");
+let nextPage = document.querySelector("#nextPage");
+
+const nextpageURL = "../fourth-game/fourthgame.html";
+let textAry = ["这是哪儿？","一切都是黑乎乎的...","外面有动静！"];
+
+const typeDelay = 100;
+const startDelay = 1000;
+
+let textIdx = 0;
+let charIdx = 0;
+
+
+function type(){
+
+    if (textIdx < textAry.length){
+
+        if(charIdx < textAry[textIdx].length){
+
+            typeWrite.innerHTML += textAry[textIdx][charIdx];
+            charIdx++;
+            setTimeout(type, typeDelay);
+        }
+    
+        else{
+            
+            textIdx++;
+            charIdx = 0;
+            typeWrite.innerHTML += '<br/>';
+            setTimeout(type, typeDelay);
+    
+        }
+
+    }
+
+
+    else{
+
+        setTimeout(appearText, startDelay);
+    }
+
+     
+
+}
+
+
+
+
+function appearText(){
+
+    nextPage.style.display = "block";
+
+    document.addEventListener("click", function(){
+        window.location.href = nextpageURL;
+    });
+
+    
+     
+}
+
+document.addEventListener("DOMContentLoaded",function() {
+
+    setTimeout(type, startDelay);
+
+});
+
+
+
+
+
+
